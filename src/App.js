6c8bfe4ccc369883bb91b84b4b728781f21d02f0
Taran Rickard <tbr5094@psu.edu>
2021-01-13 11:54:05 -0500

    react app started

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App Started Succesfully
         </p>
         <a
           className="App-link"
